c38232723be4be745b05c83c727c160472b87335
wangjicong <wangjicong6403660@126.com>
2017-02-17 17:02:34 +0800

    add storageutils.java

---
+++ libray/src/main/java/wjc/tools/library/StorageUtils.java
@@ -0,0 +1,16 @@
+package wjc.tools.library;
+
+import android.os.Environment;
+import android.os.storage.StorageManager;
+import android.os.storage.StorageVolume;
+import android.util.Log;
+
+import java.io.File;
+
+/**
+ * Created by Administrator on 2017/1/23 0023.
+ */
+public class StorageUtils {
+    private static final String TAG = StorageUtils.class.getName();
+    
+}
